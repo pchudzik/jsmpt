9bc0b0eb34cf15fbe8c179a9af7904c7c32c6310
Pawel Chudzik <pawel.chudzik@gmail.com>
2014-04-17 09:49:04 +0200

    ClientContextTests

---
+++ src/test/java/com/pchudzik/jsmtp/server/nio/pool/client/ClientContextTest.java
@@ -0,0 +1,46 @@
+package com.pchudzik.jsmtp.server.nio.pool.client;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by pawel on 17.04.14.
+ */
+public class ClientContextTest {
+	private final String key = "anyKey";
+	private ClientContext context;
+
+	@BeforeMethod
+	public void setupContext() {
+		context = new ClientContext();
+	}
+
+	@Test
+	public void shouldClearPreviouslyStoredObjectsOnPut() {
+		context.put(key, "object");
+
+		context.put(key, "otherObject");
+
+		assertThat(context.getObjects(key))
+				.containsOnly("otherObject");
+	}
+
+	@Test
+	public void shouldReturnEmptyOptionalWhenObjectIsMissing() {
+		assertThat(context.getObject(key).isPresent()).isFalse();
+	}
+
+	@Test
+	public void shouldReturnEmptyListWhenObjectsAreNotSet() {
+		assertThat(context.getObjects(key)).isEmpty();
+	}
+
+	@Test
+	public void shouldReturnOptionalFromObjectWhenObjectIsPresent() {
+		context.put(key, "object");
+
+		assertThat(context.getObject(key).isPresent()).isTrue();
+	}
+}
