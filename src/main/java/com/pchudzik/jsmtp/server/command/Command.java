06e30df96f5fe887dea8519e123506eb5bf76b06
Paweł Chudzik <pawel.chudzik@gmail.com>
2014-04-18 21:01:39 +0200

    null command is not accepted

--- src/main/java/com/pchudzik/jsmtp/server/command/Command.java
+++ src/main/java/com/pchudzik/jsmtp/server/command/Command.java
@@ -1,5 +1,8 @@
 package com.pchudzik.jsmtp.server.command;
 
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang.StringUtils;
+
 /**
  * User: pawel
  * Date: 13.04.14
@@ -9,6 +12,7 @@ public class Command {
 	private final String commandString;
 
 	public Command(String commandString) {
+		Preconditions.checkArgument(StringUtils.isNotBlank(commandString));
 		this.commandString = commandString;
 	}
 
