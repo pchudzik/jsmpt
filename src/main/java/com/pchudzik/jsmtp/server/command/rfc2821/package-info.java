5ed5520ef436a5f1368d2ec1f1da7b131d92ed20
Pawel Chudzik <pawel.chudzik@gmail.com>
2014-06-02 09:55:04 +0200

    package-info with rfc url

---
+++ src/main/java/com/pchudzik/jsmtp/server/command/rfc2821/package-info.java
@@ -0,0 +1,6 @@
+package com.pchudzik.jsmtp.server.command.rfc2821;
+
+/**
+ * implementation of
+ * <a href="http://www.ietf.org/rfc/rfc2821.txt">http://www.ietf.org/rfc/rfc2821.txt</a>
+ */
---
+++ src/main/java/com/pchudzik/jsmtp/server/command/rfc821/package-info.java
@@ -0,0 +1,6 @@
+package com.pchudzik.jsmtp.server.command.rfc821;
+
+/**
+ * implementation of
+ * <a href="http://www.ietf.org/rfc/rfc0821.txt">http://www.ietf.org/rfc/rfc0821.txt</a>
+ */
